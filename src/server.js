fef40af1d7d81e8d8d39545497053acaba86f001
Joseph <joseph.kal@outlook.com>
2019-08-06 17:32:23 -0300

    tindev api

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes');
+const server = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-sjnab.mongodb.net/omnistacktindev?retryWrites=true&w=majority', {
+  useNewUrlParser: true
+});
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
